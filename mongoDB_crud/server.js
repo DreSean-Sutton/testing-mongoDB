812b93958f7aa578aaaf6f70a1bb2e9a373cfcbd
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-04 00:05:17 -0700

    Complete backend portion of tutorial

---
+++ mongoDB_crud/server.js
@@ -0,0 +1,50 @@
+require('./models/db');
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const path = require('path');
+const handleBars = require('handlebars');
+const exphbs = require('express-handlebars');
+
+const url = "mongodb://localhost:27017/test_student_management";
+
+MongoClient.connect(url, (err, db) => {
+  if(err) throw err;
+  console.log('Database created!');
+  db.close();
+});
+
+const {
+  allowInsecurePrototypeAccess
+} = require("@handlebars/allow-prototype-access");
+const bodyparser = require("body-parser");
+const studentController = require("./controllers/studentController");
+const app = express();
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+app.get("/", (req, res) => {
+  res.send(`
+  <h2>Welcome to Students Database!!</h2>
+
+  <h3>Click here to get access to the <b> <ahref="/student/list">Database</a> </b></h3>`);
+
+});
+
+app.set("views", path.join(__dirname, "/views/"));
+
+app.engine(
+  "hbs",
+  exphbs({
+    handlebars: allowInsecurePrototypeAccess(handleBars),
+    extname: "hbs",
+    defaultLayout: "mainLayout",
+    layoutsDir: __dirname + "/views/layouts/"
+  })
+);
+
+app.set("view engine", "hbs");
+app.use("/student", studentController);
+
+app.listen(3000, () => {
+  console.log("Express listening at port: 3000");
+});
